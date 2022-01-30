ad36d0b48196c46789a1dea33c3af5d416b55b37
reuben.he <reuben.he@ambergroup.io>
2022-01-30 19:51:50 +0800

    :constuction: file.go编写中

---
+++ pkg/logging/file.go
@@ -0,0 +1,14 @@
+package logging
+
+import "fmt"
+
+var (
+	LogSavePath = "runtime/logs/"
+	LogSaveName = "log"
+	LogFileExt  = "log"
+	TimeFormat  = "20060102"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
